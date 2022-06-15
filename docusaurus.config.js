8bde0daa855e4c71085a42c92c8e33dc6722caf6
gpgun0 <rjsgh7943@gmail.com>
2022-06-15 19:54:24 +0900

    modify url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Weekly Atelier",
   tagline: "My Blog",
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://gpgun0.github.io",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
